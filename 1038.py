f81d3eb4a2a69c944bf45458a85eb1a3da78ee4b
Guilherme Vinicius Lima Sousa <55168525+viniciusexe@users.noreply.github.com>
2021-08-06 15:39:41 -0300

    Update 1038.py

--- 1038.py
+++ 1038.py
@@ -1,13 +1,12 @@
-A,B = input().split()
-A = int(A)
-B = int(B)
+C = input().split()
+A, B = C
 if A == '1':
-    print("Total: R$ {:.2}".format(4.00*float(B)))
+    print("Total: R$ {:.2f}".format(4.00*float(B)))
 if A == '2':
-    print("Total: R$ {:.2}".format(4.50*float(B)))
+    print("Total: R$ {:.2f}".format(4.50*float(B)))
 if A == '3':
-    print("Total: R$ {:.2}".format(5.00*float(B)))
+    print("Total: R$ {:.2f}".format(5.00*float(B)))
 if A == '4':
-    print("Total: R$ {:.2}".format(2.00*float(B)))
+    print("Total: R$ {:.2f}".format(2.00*float(B)))
 if A == '5':
-    print("Total: R$ {:.2}".format(1.50*float(B)))
+    print("Total: R$ {:.2f}".format(1.50*float(B)))
