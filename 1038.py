084908d27f352cba899e2ae88ecb813045c8fe0a
Guilherme Vinicius Lima Sousa <55168525+viniciusexe@users.noreply.github.com>
2021-08-06 19:08:27 -0300

    Update 1038.py

--- 1038.py
+++ 1038.py
@@ -1,12 +1,23 @@
-C = input().split()
-A, B = C
-if A == '1':
-    print("Total: R$ {:.2f}".format(4.00*float(B)))
-if A == '2':
-    print("Total: R$ {:.2f}".format(4.50*float(B)))
-if A == '3':
-    print("Total: R$ {:.2f}".format(5.00*float(B)))
-if A == '4':
-    print("Total: R$ {:.2f}".format(2.00*float(B)))
-if A == '5':
-    print("Total: R$ {:.2f}".format(1.50*float(B)))
+a, b = input().split(' ')
+a = int(a)
+b = int(b)
+l1 = 1
+l2 = 2
+l3 = 3
+l4 = 4
+l5 = 5
+if a == l1:
+    t = 4.00 * b
+    print(f'Total: R$ {t:.2f}')
+elif a == l2:
+    t = 4.50 * b
+    print(f'Total: R$ {t:.2f}')
+elif a ==l3:
+    t = 5.00 * b
+    print(f'Total: R$ {t:.2f}')
+elif a == l4:
+    t = 2.00 * b
+    print(f'Total: R$ {t:.2f}')
+elif a == l5:
+    t = 1.50 * b
+    print(f'Total: R$ {t:.2f}')
