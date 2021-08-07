a37d96d27696268bab5159596e8ed53650cfffd8
Guilherme Vinicius Lima Sousa <55168525+viniciusexe@users.noreply.github.com>
2021-08-07 07:48:25 -0300

    Create 1010.py

---
+++ 1010.py
@@ -0,0 +1,8 @@
+a1, b1, c1 = input().split(' ')
+a2, b2, c2 = input().split(' ')
+b1 = int(b1)
+c1 = float(c1)
+b2 = int(b2)
+c2 = float(c2)
+c = (b1 * c1) + (b2 *c2)
+print(f"VALOR A PAGAR: R$ {c:.2f}")
